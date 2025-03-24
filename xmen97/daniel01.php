10df191058072464af258306e687cc38dc4f014e
Tomas Garza Moya <tomasgarzamoya@Tomass-MacBook-Air-2.local>
2025-03-24 13:38:09 -0500

    Relaciones 05b. Tienda

--- xmen97/daniel01.php
+++ xmen97/daniel01.php
@@ -33,6 +33,87 @@ ini_set('display_errors', 1);
         <title>Pagina alterna de prueba</title>
     </head>
     <body>
+    <style>
+    h1{
+        font-size:12px;
+      text-align:center;
+      color:#334139;
+      margin-bottom:20px;
+    }
+    table{
+      width: 100%;
+      border-collapse: collapse;
+      margin-top:50px;
+      border-radius:50px;
+    }
+    th, td{
+      padding:10px;
+      text-align:left;
+      border-bottom: 1px solid #ddd;
+    }
+    tr:nth-child(even){
+      background-color:#F9B4ED;
+      color:black;
+    }
+    tr:nth-child(odd){
+      background-color:white;
+      color:black;
+    }
+    th{
+      background-color:#C52184;
+      color:white;
+    }
+  </style>
+  <style>
+    .container1{
+      justify-content:center;
+      align-items:center;
+      width:50%;
+      background-color:#282a36;
+      padding: 20px;
+      border-radius:10px;
+      box-shadow: 0 0 10px rgba(0,0,0,0.2);
+      color:white;
+    }
+    h1, h2{
+      font-family: 'so this is it', sans-serif; 
+      text-shadow: 0 1 1 black;
+      text-align:center;
+      color:#ff79c6;
+      margin-bottom:15px;
+      font-size:15px;
+    }
+    form{
+      display:flex;
+      flex-direction:column;
+    }
+    label{
+      font-size:16px;
+      margin-bottom:5px;
+    }
+    input[type="text"] {
+    padding: 8px;
+    margin-bottom: 10px;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    background-color: #44475a;
+    color: #fff;
+}
+input[type="submit"] {
+    padding: 10px;
+    background-color: #50fa7b;
+    border: none;
+    color: #282a36;
+    font-size: 16px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background 0.3s;
+}
+input[type="submit"]:hover {
+    background-color: #3ae374;
+}
+  </style>
         <h1>Registrar Productos</h1>
         <form method = "POST">
             <label>Nombre del producto: </label>
@@ -50,7 +131,34 @@ ini_set('display_errors', 1);
                 }
                 ?>
             </select><br><br>
-
+            <input type="submit" value="Agregar producto">
         </form>
+        <h2>Lista de Productos</h2>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Categoria</th>
+            </tr>
+            <?php
+            $sql_productos = "SELECT productos.nombre, productos.precio, categorias.nombre AS categoria 
+            FROM productos 
+            JOIN categorias ON productos.id_categoria = categorias.id";
+            
+            $result_productos = $conexion->query($sql_productos);
+            if($result_productos->num_rows>0){
+                while($row = $result_productos -> fetch_assoc()){
+                    echo "<tr>
+                        <th>{$row['nombre']}</th>
+                        <th>{$row['precio']}</th>
+                        <th>{$row['categoria']}</th>
+                    
+                    </tr>";
+                }
+            }else{
+                echo "<tr><td>No hay productos registrados</td></tr>";
+            }
+            ?>
+        </table>
     </body>
 </html>
--- xmen98/daniel01.php
+++ xmen98/daniel01.php
@@ -32,6 +32,87 @@
                     <title>Pagina de Prueba</title>
                 </head>
                 <body>
+                <style>
+    h1{
+        font-size:12px;
+      text-align:center;
+      color:#334139;
+      margin-bottom:20px;
+    }
+    table{
+      width: 100%;
+      border-collapse: collapse;
+      margin-top:50px;
+      border-radius:50px;
+    }
+    th, td{
+      padding:10px;
+      text-align:left;
+      border-bottom: 1px solid #ddd;
+    }
+    tr:nth-child(even){
+      background-color:#F9B4ED;
+      color:black;
+    }
+    tr:nth-child(odd){
+      background-color:white;
+      color:black;
+    }
+    th{
+      background-color:#C52184;
+      color:white;
+    }
+  </style>
+  <style>
+    .container1{
+      justify-content:center;
+      align-items:center;
+      width:50%;
+      background-color:#282a36;
+      padding: 20px;
+      border-radius:10px;
+      box-shadow: 0 0 10px rgba(0,0,0,0.2);
+      color:white;
+    }
+    h1, h2{
+      font-family: 'so this is it', sans-serif; 
+      text-shadow: 0 1 1 black;
+      text-align:center;
+      color:#ff79c6;
+      margin-bottom:15px;
+      font-size:15px;
+    }
+    form{
+      display:flex;
+      flex-direction:column;
+    }
+    label{
+      font-size:16px;
+      margin-bottom:5px;
+    }
+    input[type="text"] {
+    padding: 8px;
+    margin-bottom: 10px;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    background-color: #44475a;
+    color: #fff;
+}
+input[type="submit"] {
+    padding: 10px;
+    background-color: #50fa7b;
+    border: none;
+    color: #282a36;
+    font-size: 16px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background 0.3s;
+}
+input[type="submit"]:hover {
+    background-color: #3ae374;
+}
+  </style>
                     <h2>Registrar productos</h2>
                     <form method="POST">
                         <label>Nombre del producto:</label>
